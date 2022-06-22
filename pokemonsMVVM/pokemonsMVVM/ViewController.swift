5d6fa2478b3c770fbd4853beb11a9ad5b810700c
Ivan Pavic <ip7594@gmail.com>
2022-06-22 11:50:47 +0200

    First commit

---
+++ pokemonsMVVM/pokemonsMVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  pokemonsMVVM
+//
+//  Created by Ivan Pavic on 22.6.22..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
